e23050df6979510dde66be0f24f4fedc4dfbf97b
Rachel Lilja <bmax98@MacBook-Pro.local>
2019-12-10 17:08:48 -0800

    Deleted Python files and paths.

---
+++ Amazon_product_scrapper.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import cherrio from 'cheerio';
+
+async function getHTML(productURL) {
+  const { data: html } = await  axios.get(productURL, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  return html;
+}
+
+async function getAmazonPrice(html) {
+  const $ = cherrio.load(html)
+  
+  const span = $('#priceblock_saleprice')
+  return span.html();
+}
+
+export { getHTML, getAmazonPrice };
